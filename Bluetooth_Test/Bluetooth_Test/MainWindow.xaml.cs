b74264c879b1a54aeaa8e4b93563c9579ddf09ec
Giovanni Allemeersch <giovanni.allemeersch@gmail.com>
2017-12-29 00:58:00 +0100

    added the project and updated the readme

---
+++ Bluetooth_Test/Bluetooth_Test/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System.Windows;
+using InTheHand.Net.Sockets;
+using System;
+using InTheHand.Net;
+using System.Collections.Generic;
+using System.Linq;
+using InTheHand.Net.Bluetooth;
+
+// I know that this can all be so much better, but this is a tinkering project, I made it to learn from it...
+// Later on I might make this decently coded, but I'm not sure yet...
+//Author: Giovanni Allemeersch
+
+namespace Bluetooth_Test
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        BluetoothDeviceInfo[] devices;
+        List<BluetoothDeviceInfo> bluetoothDeviceInfos = new List<BluetoothDeviceInfo>();
+        BluetoothEndPoint ble;
+        public MainWindow()
+        {
+            InitializeComponent();
+            BluetoothClient device = new BluetoothClient();
+            device.BeginDiscoverDevices(8, true, true, true, true, ScanDevices, device);
+            
+        }
+
+        /// <summary>
+        /// Method to scan for the devices present
+        /// </summary>
+        /// <param name="result"></param>
+        private void ScanDevices(IAsyncResult result)
+        {
+            //Get back the BluetoothClient which was passed as the state parameter in BeginDiscoverDevices
+            BluetoothClient thisDevice = result.AsyncState as BluetoothClient;
+            //Check if the scanning is completed
+            if (result.IsCompleted)
+            {
+                //get the list of devices
+               devices = thisDevice.EndDiscoverDevices(result);
+                 //add the devices to the listbox
+                foreach(BluetoothDeviceInfo b in devices)
+                {
+                    this.Dispatcher.Invoke(() => {
+                        lbItems.Items.Add(b.DeviceName.ToString());
+                        bluetoothDeviceInfos.Add(b);
+                    });
+                }
+            }
+        }
+
+        /// <summary>
+        /// Event handler for the selection changed event in the listbox
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void lbItems_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            var b = from a in bluetoothDeviceInfos where a.DeviceName == lbItems.SelectedItem.ToString() select a;
+            
+            foreach(var c in b)
+            {
+                bool test = BluetoothSecurity.PairRequest(c.DeviceAddress, "01234");
+                if (test)
+                    MessageBox.Show("connected");
+                else
+                    MessageBox.Show("could not connect :(");
+            }
+            //BluetoothClient blc = new BluetoothClient();
+
+       
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Bluetooth_Experiment
 just tinkering around with bluetooth in C#, not going anywhere with it... at least not yet
+
+***Note: I don't know how much time I will invest in this, since I have finals shortly... I might continue or I might not ;), exciting isn't it?***
